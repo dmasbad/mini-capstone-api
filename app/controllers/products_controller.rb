ac9f5c2317c8a0dbdc4ff18b02627755d45f57dc
Daniel Mabad <djwmasbad@gmail.com>
2024-05-15 14:05:19 -0400

    post

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,8 +5,14 @@ class ProductsController < ApplicationController
   end
 
   def show
-    p params[:id]
+    params[:id]
     @product = Product.find_by(id: params[:id])
+    render template: "products/show"
+  end
+
+  def create
+    @product = Product.new(name: "pencil sharpener", price: 6, description: "long-point")
+    @product.save
     render template: "products/show"
   end
 end
